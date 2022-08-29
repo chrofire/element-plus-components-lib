e58cd6c06e905b0e0b55549aa96b41368106845c
chrofire <451939778@qq.com>
2022-08-29 21:52:51 +0800

    init: 初始化

---
+++ .stylelintrc.cjs
@@ -0,0 +1,34 @@
+/* eslint-env node */
+/** @type {import('stylelint').Config} */
+module.exports = {
+    processors: [],
+    extends: [
+        // 拓展 stylelint 兼容 scss
+        'stylelint-config-standard-scss',
+        // 拓展 stylelint 兼容 vue (依赖 postcss-html)
+        'stylelint-config-recommended-vue'
+    ],
+    ignoreFiles: ['node_modules/**/*', 'dist/**/*'],
+    rules: {
+        indentation: 4,
+        'rule-empty-line-before': [
+            'always',
+            {
+                except: [
+                    'after-rule',
+                    'after-single-line-comment',
+                    'inside-block-and-after-rule',
+                    'inside-block',
+                    'first-nested'
+                ]
+            }
+        ],
+        'no-empty-source': null,
+        'block-no-empty': null,
+        'selector-pseudo-class-no-unknown': null,
+        'selector-pseudo-element-no-unknown': null,
+        'no-duplicate-selectors': null,
+        'at-rule-no-unknown': null,
+        'selector-class-pattern': null
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# element-plus-components-lib
