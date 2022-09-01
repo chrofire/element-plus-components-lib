51bb5f6fe8501c558bbf710c0338762a7c0c8010
chrofire <451939778@qq.com>
2022-09-01 23:01:16 +0800

    build: 支持按需导入

--- vite.config.js
+++ vite.config.js
@@ -14,22 +14,34 @@ export default defineConfig({
     },
     build: {
         lib: {
+            // umd | iife, 挂载到 window 上的属性名
+            name: 'ElementPlusComponentsLib',
+            // 入口
             entry: fileURLToPath(
                 new URL('./packages/element-plus-components-lib/index.js', import.meta.url)
-            ),
-            name: 'element-plus-components-lib',
-            fileName: 'index'
+            )
         },
         rollupOptions: {
-            // 外部化处理不打包进库的依赖
+            // 作为外部依赖, 不打包
             external: ['vue', 'element-plus'],
-            output: {
-                exports: 'named',
-                globals: {
-                    vue: 'Vue',
-                    'element-plus': 'ElementPlus'
+            output: [
+                {
+                    format: 'es',
+                    // 输出文件名
+                    entryFileNames: '[name].js',
+                    // 输出目录
+                    dir: fileURLToPath(new URL('./dist/es', import.meta.url)),
+                    // 保持模块结构
+                    preserveModules: true
+                },
+                {
+                    format: 'cjs',
+                    entryFileNames: '[name].js',
+                    dir: fileURLToPath(new URL('./dist/lib', import.meta.url)),
+                    preserveModules: true,
+                    exports: 'named'
                 }
-            }
+            ]
         }
     }
 })
